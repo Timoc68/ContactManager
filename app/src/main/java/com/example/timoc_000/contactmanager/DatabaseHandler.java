3821d2ae5dc845841af4b132c3828bc789011878
Timoc68 <timocall68@gmail.com>
2015-05-13 21:46:40 +1000

    Initial commit

---
+++ app/src/main/java/com/example/timoc_000/contactmanager/DatabaseHandler.java
@@ -0,0 +1,130 @@
+package com.example.timoc_000.contactmanager;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by timoc_000 on 12/05/2015.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactManager";
+    private static final String TABLE_CONTACTS = "contacts";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE = "phone";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_ADDRESS = "address";
+    private static final String KEY_IMAGEURI = "imageUri";
+
+    public DatabaseHandler(Context context) {
+        super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID + " integer primary key autoincrement, " +
+                KEY_NAME + " TEXT, " +
+                KEY_PHONE + " TEXT, " +
+                KEY_EMAIL + " TEXT, " +
+                KEY_ADDRESS + " TEXT, " +
+                KEY_IMAGEURI + " TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+
+        onCreate(db);
+    }
+
+    public void createContact(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PHONE, contact.getPhone());
+        values.put(KEY_EMAIL, contact.getEmail());
+        values.put(KEY_ADDRESS, contact.getAddress());
+        values.put(KEY_IMAGEURI, contact.getImageURI().toString());
+
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID, KEY_NAME, KEY_PHONE, KEY_EMAIL, KEY_ADDRESS, KEY_IMAGEURI }, KEY_ID + "=?", new String[] { String.valueOf(id) }, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), Uri.parse(cursor.getString(5)));
+        cursor.close();
+        db.close();
+
+        return (contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.delete(TABLE_CONTACTS, KEY_ID + "=?", new String[]{String.valueOf(contact.getId())});
+        db.close();
+    }
+
+    public int getContactsCount() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE_CONTACTS, null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        return(count);
+    }
+
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PHONE, contact.getPhone());
+        values.put(KEY_EMAIL, contact.getEmail());
+        values.put(KEY_ADDRESS, contact.getAddress());
+        values.put(KEY_IMAGEURI, contact.getImageURI().toString());
+
+        int rowsAffected = db.update(TABLE_CONTACTS, values, KEY_ID + "=?", new String[]{String.valueOf(contact.getId())});
+        db.close();
+
+        return (rowsAffected);
+    }
+
+    public List<Contact> getAllContacts() {
+        List<Contact> contacts = new ArrayList<Contact>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_CONTACTS, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                contacts.add(new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), Uri.parse(cursor.getString(5))));
+            }
+            while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+
+        return(contacts);
+    }
+}
